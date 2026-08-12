3551d7da986cb0f307aad4205451f5bd34d5409d
Seo yeon Kim <merock2tar@gmail.com>
2026-08-12 10:00:56 +0900

    자바 프로그램 개발 과정

---
+++ studyjava/chap01/src/sex03/exam01/Hello.java
@@ -0,0 +1,16 @@
+package sex03.exam01;
+
+/*
+ * 작성자: 김서연
+ * 작성일: 2026.08.12
+ */
+
+// 프로그램 실행 진입점
+public class Hello {
+	
+	public static void main(String[] args) {
+		// 콘솔에 출력하는 실행문 
+		System.out.println("Hello, World!");
+	}
+
+}
